6964bc63e71db551ab785bcff3017158f95dacf6
isAllyn <2282249330@qq.com>
2022-11-24 08:28:13 +0800

    2022年11月24日- 8时28分13秒- 周四

---
+++ lib/拉取.bat
@@ -0,0 +1,7 @@
+@echo off
+cd Super-series
+git init
+git remote add origin git@github.com:mfxhb/Super-series.git
+git remote add gitee git@gitee.com:mfxhb/super-series.git
+git pull -r origin master
+pause
---
+++ lib/提交.bat
@@ -0,0 +1,15 @@
+@echo off
+set year=%date:~0,4%
+set mon=%date:~5,2%
+set day=%date:~8,2%
+set zhou=%date:~10,6%
+set h=%time:~0,2%
+set m=%time:~3,2%
+set s=%time:~6,2%
+set comtxt=%year%年%mon%月%day%日-%h%时%m%分%s%秒-%zhou%
+cd Super-series
+git add .
+git commit -m "%comtxt%"
+git push origin master
+git push gitee master
+pause
